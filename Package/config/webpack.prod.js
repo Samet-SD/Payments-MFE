94cd04363d80cafc3d67216b7620db5bbc2fa3c6
Samet Demko <demko.samet@gmail.com>
2023-01-06 15:19:11 +0100

    production ready

--- Package/config/webpack.prod.js
+++ Package/config/webpack.prod.js
@@ -7,7 +7,8 @@ const packageJson = require('../package.json');
 const prodConfig = {
     mode: 'production', 
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/payments/latest/'
     },
    plugins: [
     new ModuleFederationPlugin({
